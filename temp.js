a7a04c0102572ded2036db947a48cf248a8fb922
Ryan <rwolfe22@gmail.com>
2015-08-10 14:46:05 -0400

    more updates

--- temp.js
+++ temp.js
@@ -1,30 +1,69 @@
 
 function init(){
+  $(".quote").fadeToggle(1000);
 
-  $( ".quote" ).click(function() {
-    $( ".quote" ).fadeToggle("slow");
-    $( ".great" ).fadeToggle("slow");
+  $( document.body ).click(function() {
+      $( ".quote" ).fadeOut(1250);
+      setTimeout(function(){
+        $( "#one" ).fadeIn();
+        $( "#two" ).fadeIn();
+        $( "#three" ).fadeIn();
+        $( "#four" ).fadeIn();
+      }, 1000);
   });
 
-  /*
-  var h1tags = document.getElementsByTagName("h1");
-  var h2tags = document.getElementsByTagName("h2");
-  var h3tags = document.getElementsByTagName("h3");
+  //menu 1
+  $("#menu").mouseenter(function(){
+     $("#menu").animate({
+            height: '+=200',
+        });
+  });
+  $("#menu").mouseleave(function(){
+     $("#menu").animate({
+            height: '-=200',
+        });
+  });
+  ////////
+
+  //menu 2
+  $("#menut").mouseenter(function(){
+     $("#menut").animate({
+            height: '+=200',
+        });
+  });
+  $("#menut").mouseleave(function(){
+     $("#menut").animate({
+            height: '-=200',
+        });
+  });
+  ///////
+
+  //menu 3
+  $("#menuth").mouseenter(function(){
+     $("#menuth").animate({
+            height: '+=200',
+        });
+  });
+  $("#menuth").mouseleave(function(){
+     $("#menuth").animate({
+            height: '-=200',
+        });
+  });
+  /////
 
 
-  h1tags[0].onclick = changeColor;
-  h2tags[0].onclick = changeColor;
-  h3tags[0].onclick = fadeOut;
-  */
-}
-
-function changeColor(){
-  //this.innerHTML = "Click Again";
-
-  var randomcolor = '#'+Math.floor(Math.random()*16777215).toString(16);
-
-  this.style.color = randomcolor;
+  //menu 4
+  $("#menuf").mouseenter(function(){
+     $("#menuf").animate({
+            height: '+=200',
+        });
+  });
+  $("#menuf").mouseleave(function(){
+     $("#menuf").animate({
+            height: '-=200',
+        });
+  });
+  /////
 }
 
 onload=init;
-
